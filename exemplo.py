8c9650569f55153f59a6398f1e776b6bbce07098
Roberto Slawinski <69437022+robertoslawinski@users.noreply.github.com>
2025-10-28 11:36:16 +0000

    Refactor argument handling in exemplo.py

--- exemplo.py
+++ exemplo.py
@@ -1,11 +1,14 @@
-import os, sys
-
+import sys
 print("Nome do script:", sys.argv[0])
 print("Argumentos:", sys.argv[1:])
 print("Número de argumentos:", len(sys.argv) - 1)
 
-if len(sys.argv) >= 4:
-    nome, apelido, cidade, ambiente = sys.argv[1:5]
-    print(f"nome={nome}, apelido={apelido}, cidade={cidade}, ambiente={ambiente}")
-else:
-    print("⚠️ Dica: rode com 4 argumentos: <nome> <apelido> <cidade> <ambiente>")
+if len(sys.argv) < 5:
+    print("⚠️ Dica: rode com 4 argumentos: <nome> <apelido> <confirme> <cidade>")
+    sys.exit(0)
+
+nome, apelido, confirme, cidade = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+print("nome =", nome)
+print("apelido =", apelido)
+print("confirme =", confirme)
+print("cidade =", cidade)
