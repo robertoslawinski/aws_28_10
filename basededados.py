dfd34a10a154b010f5c0dc073af3b648a5f67a1d
Roberto Slawinski <69437022+robertoslawinski@users.noreply.github.com>
2025-10-28 12:00:32 +0000

    Fix print statement for database connection message

--- basededados.py
+++ basededados.py
@@ -3,7 +3,7 @@ import yaml
 with open("db.yml", "r") as f:
     config = yaml.safe_load(f)
 
-print("📌 Conectando ao Banco de Dados...")
+print("Conectando ao Banco de Dados...")
 print("Servidor:", config["servidor"])
 print("Utilizador:", config["utilizador"])
 print("Base de Dados:", config["base_de_dados"])
