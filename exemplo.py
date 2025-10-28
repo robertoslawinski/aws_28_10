112443bf57cea632997cce6dce08d3bd02ffa3e9
Roberto Slawinski <69437022+robertoslawinski@users.noreply.github.com>
2025-10-28 12:01:14 +0000

    Update warning message for argument requirements

--- exemplo.py
+++ exemplo.py
@@ -4,7 +4,7 @@ print("Argumentos:", sys.argv[1:])
 print("Número de argumentos:", len(sys.argv) - 1)
 
 if len(sys.argv) < 5:
-    print("⚠️ Dica: rode com 4 argumentos: <nome> <apelido> <confirme> <cidade>")
+    print("Rodar com 4 argumentos: <nome> <apelido> <confirme> <cidade>")
     sys.exit(0)
 
 nome, apelido, confirme, cidade = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
